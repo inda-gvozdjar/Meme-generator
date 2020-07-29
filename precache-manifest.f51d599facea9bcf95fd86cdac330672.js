0fa0dee6a8b958f56712fb513c70ebab2af1e426
Inda Gvozdjar <inda.gvozdjar@gmail.com>
2020-07-30 00:28:15 +0200

    Updates

---
+++ precache-manifest.f51d599facea9bcf95fd86cdac330672.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae6b847ddd9a8bb2475ce88cf510fce5",
+    "url": "/Meme-generator/index.html"
+  },
+  {
+    "revision": "d7eef34cb5c53ba24ffe",
+    "url": "/Meme-generator/static/js/2.98141b07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Meme-generator/static/js/2.98141b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c32de0f30caf1b2f6298",
+    "url": "/Meme-generator/static/js/main.f2f40fd0.chunk.js"
+  },
+  {
+    "revision": "0675073108bde07d57ac",
+    "url": "/Meme-generator/static/js/runtime-main.d28ab900.js"
+  }
+]);
